b926e00caa9740d2ff95316a7c02f3802af2713b
Parker Bronson <parbro3@gmail.com>
2020-07-09 18:38:02 -0600

    Completely changed backend. renamed api and auth. added withdraw, deposit, and count endpoints

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify, { API } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
 function App() {
   return (
     <div className="App">
